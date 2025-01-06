6bc1f879f340f5406a7b2db04ac3ec47362f73b8
katas8n1 <kirka6061512@icloud.com>
2025-01-06 20:29:57 +0100

    added settings

---
+++ src/main/java/com/example/commonProject/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.commonProject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/admin/**")
+                        .authenticated()
+                        .anyRequest()
+                        .permitAll()
+
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .rememberMe(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/example/commonProject/dto/ProductDto.java
@@ -0,0 +1,11 @@
+package com.example.commonProject.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDto {
+    private String name;
+    private double price;
+    private  String description;
+    private String imageUrl;
+}
