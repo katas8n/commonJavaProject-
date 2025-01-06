01a9a701c82e69b9642900655bcfca35a59f6d2c
katas8n1 <kirka6061512@icloud.com>
2025-01-06 18:30:00 +0100

    First commit

---
+++ src/test/java/com/example/commonProject/CommonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commonProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
